230659ffc636db791b8250ac9a1d3e77d8fabf45
Raphael Karanja <rkaranja@cytonn.com>
2018-07-16 15:38:33 +0300

    add component to add role permissions

--- resources/assets/js/components/pages/users/view_role.vue
+++ resources/assets/js/components/pages/users/view_role.vue
@@ -7,6 +7,10 @@
             getRole(){
                 let role_id = this.$route.params.roleId;
                 this.$store.commi('GET_ROLE_DETAILS', role_id)
+            },
+            addPermission(){
+                let role_id = this.$route.params.roleId;
+                this.$router.push({name: 'add_permissions_to_role', params: { roleId }})
             }
         }
     }
@@ -14,7 +18,7 @@
 <template>
     <div class="role_details">
         <div class="role_details__head">
-            
+            <el-button type="primary" @click="addPermission" >Add Permission to role</el-button>
         </div>
         <div class="role_details__body">
 
